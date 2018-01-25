efde09ee73fa7e34f33dd1cf8a25243db89ffeaa
jipenfei <921374010@qq.com>
2018-01-25 21:38:16 +0800

    哈哈

--- 综合题/ps切图/script.js
+++
@@ -1,3 +0,0 @@
-/**
- * Created by ASUS-PC on 2018/1/13.
- */
--- 综合题/requireJs/isArray.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by ASUS-PC on 2017/12/10.
- */
-define(function() {
-    function isArray(arr){
-        return (arr instanceof  Array && arr.constructor == Array);
-    }
-    return isArray;
-})
